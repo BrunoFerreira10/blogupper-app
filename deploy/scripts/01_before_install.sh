44da53080a4e2e4432c7c1d358c3e408cf1c9847
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-10 11:30:01 -0300

    Adicionado .gitignore

--- deploy/scripts/01_before_install.sh
+++ deploy/scripts/01_before_install.sh
@@ -3,5 +3,11 @@
 echo "------ Parar aplicação ------"
 systemctl stop apache2
 sudo a2enmod rewrite
-
-
+# sudo a2enmod ssl # Usando via ACM no ELB
+sudo a2enmod headers
+sudo a2enmod expires
+sudo a2enmod deflate
+sudo a2enmod mime
+sudo a2enmod dir
+sudo a2enmod env
+sudo a2enmod filter
